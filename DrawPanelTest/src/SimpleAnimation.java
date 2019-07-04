b7c281aadae5e34542a418d483895d17d6afd782
Satiel27 <52329794+Satiel27@users.noreply.github.com>
2019-07-04 18:27:29 +0400

    Drawing panel + Animation

---
+++ DrawPanelTest/src/SimpleAnimation.java
@@ -0,0 +1,62 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class SimpleAnimation {
+
+	int x = 70;
+	int y = 70;
+	
+	public static void main(String[] args) {
+	
+		SimpleAnimation qui = new SimpleAnimation();
+		qui.go();
+		
+	}
+	
+	public void go() {
+		
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		MyDrawPanel drawPanel = new MyDrawPanel();
+		
+		frame.getContentPane().add(drawPanel);
+		frame.setSize(300, 300);
+		frame.setVisible(true);
+		
+		for (int i = 0; i < 130; i++) {
+			
+			x++;
+			y++;
+			
+			drawPanel.repaint();
+			
+			try {
+				Thread.sleep(50);
+				
+			} catch (Exception ex) {
+			
+		}
+			
+	}
+}
+	
+	
+	class MyDrawPanel extends JPanel {
+		
+		public void paintComponent(Graphics q) {
+			
+			q.setColor(Color.white);
+			q.fillRect(0, 0, this.getWidth(), this.getHeight());
+			
+			q.setColor(Color.green);
+			q.fillOval(x, y, 40, 40);
+			
+		}
+		
+	}
+
+}
+
+	
+	
